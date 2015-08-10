7463d47b5436d0acb7a84319ead91eeeeaa66201
Tamas Piros <me@tamas.io>
2015-08-10 15:54:25 +0200

    updated html render

--- app.js
+++ app.js
@@ -8,15 +8,12 @@ app.set('port', 3000);
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
 app.use('/', router);
-app.engine('html', require('ejs').renderFile);
 
 var index = function(req, res) {
-  res.render('index.html');
+  res.sendFile(__dirname + '/views/index.html');
 };
 
 router.route('/').get(index);
-
-
 
 app.listen(app.get('port'));
 console.log('Magic happens on port ' + app.get('port'));
